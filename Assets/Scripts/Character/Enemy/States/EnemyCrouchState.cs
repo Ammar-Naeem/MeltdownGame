88031153def00393b5368648e62981e04301a4e8
Muhammad Ammar Naeem <ammar.naeem@tintash.com>
2023-03-26 03:31:44 +0500

    updated crouch

--- Assets/Scripts/Character/Enemy/States/EnemyCrouchState.cs
+++ Assets/Scripts/Character/Enemy/States/EnemyCrouchState.cs
@@ -1,18 +1,58 @@
 using System.Collections;
 using System.Collections.Generic;
+using MyNamespace;
 using UnityEngine;
 
-public class EnemyCrouchState : MonoBehaviour
+namespace Meltdown
 {
-    // Start is called before the first frame update
-    void Start()
+    public class EnemyCrouchState : EnemyBaseState
     {
-        
-    }
+        public override void Enter(IBaseStateMachine baseStateMachine, GameObject enemyObject)
+        {
+            base.Enter(baseStateMachine, enemyObject);
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+            Debugger.DebugLog("In crouch state");
+            
+            PlayCrouch();
+        }
+
+        public override void LogicUpdate()
+        {
+            if (Input.GetKeyUp(KeyCode.DownArrow))
+            {
+                ChangeStateToIdle();
+            }
+        }
+
+        public override void PhysicsUpdate()
+        {
+        }
+
+        public override void Exit()
+        {
+            Enemy.GetColliderTransform()
+                .localScale = Enemy.GetColliderSizeAtStart();
+        }
+
+        private void PlayCrouch()
+        {
+            AnimationController.Crouch();
+            UpdateScaleOfCollider();
+        }
+
+        private void UpdateScaleOfCollider()
+        {
+            Vector3 currentScale = Enemy.GetColliderTransform()
+                .localScale;
+            currentScale.y = 0.4f;
+            Enemy.GetColliderTransform()
+                .localScale = currentScale;
+        }
+
+        private void ChangeStateToIdle()
+        {
+            BaseStateMachine.ChangeState(Enemy.GetEnemyStateFactory()
+                .GetEnemyIdleState());
+        }
     }
 }
