016bf339e6087e3a48d20b128717736e51740a20
Muhammad Ammar Naeem <ammar.naeem@tintash.com>
2023-03-26 03:29:11 +0500

    updated jumo state

--- Assets/Scripts/Character/Enemy/States/EnemyJumpState.cs
+++ Assets/Scripts/Character/Enemy/States/EnemyJumpState.cs
@@ -1,21 +1,101 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 namespace Meltdown
 {
-    public class EnemyJumpState : MonoBehaviour
+
+    public class EnemyJumpState : EnemyBaseState
     {
-        // Start is called before the first frame update
-        void Start()
+        private const           string GroundTag        = "Ground";
+        private                 bool   _isJumpPerformed = false;
+        private static readonly int    s_jump           = Animator.StringToHash("Jump");
+
+        public override void Enter(IBaseStateMachine baseStateMachine, GameObject enemyObject)
         {
-        
+            base.Enter(baseStateMachine, enemyObject);
+
+            Debug.Log("In Jump state");
+            PerformJump();
         }
 
-        // Update is called once per frame
-        void Update()
+        public override void LogicUpdate()
         {
-        
+        }
+
+        public override void PhysicsUpdate()
+        {
+            if (!_isJumpPerformed)
+            {
+                return;
+            }
+
+            if (EnemyRigidbody.velocity.y < 0)
+            {
+                // ChangeStateToIdle();
+                ChangeStateToJumpInAirState();
+            }
+        }
+
+
+        public override void Exit()
+        {
+        }
+
+        private void PerformJump()
+        {
+            if (_isJumpPerformed)
+            {
+                return;
+            }
+
+            if (IsGrounded())
+            {
+                _isJumpPerformed = true;
+                
+                Vector3 currentScale = Enemy.GetColliderTransform().GetChild(0).localScale;
+                currentScale.y = 1f;
+                Enemy.GetColliderTransform()
+                    .GetChild(0)
+                    .localScale = currentScale;
+
+                AnimationController.Jump();
+
+                EnemyRigidbody
+                    .AddForce(Vector3.up * Enemy.GetJumpForce(), ForceMode.Impulse);
+            }
+            else
+            {
+                ChangeStateToIdle();
+            }
+        }
+
+        private bool IsGrounded()
+        {
+            Vector3 origin = Enemy.GetCharacterPositionInWorld() + Vector3.up * 0.25f;
+
+            Ray ray = new Ray(origin, Vector3.down);
+            Debug.DrawRay(origin, Vector3.down * 0.5f, Color.green, 50);
+
+            if (Physics.Raycast(ray, out RaycastHit hit, 0.5f, LayerHelper.GetStandLayerMask()))
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        private void ChangeStateToIdle()
+        {
+            _isJumpPerformed = false;
+            BaseStateMachine.ChangeState(Enemy.GetEnemyStateFactory()
+                .GetEnemyIdleState());
+        }
+
+        private void ChangeStateToJumpInAirState()
+        {
+            _isJumpPerformed = false;
+            BaseStateMachine.ChangeState(Enemy.GetEnemyStateFactory()
+                .GetEnemyJumpInAirState());
         }
     }
 }
